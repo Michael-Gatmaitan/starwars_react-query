3b0120adebee44a1c39c4e2c93209255f16706c5
Michael Gatmaitan <mchlgtmtn@gmail.com>
2021-09-22 12:27:46 +0800

    UI Complleted, porfolio link need to update ASAP.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,35 @@ const Contact = () => (
   <div className="contact-container">
     <div className="header">Contact</div>
 
-    <div className="body-text"></div>
+    <div className="body-text">
+      I'm Michael Gatmaitan from Marilao, Bulacan, Philippines and hoping to
+      land my first job in tech this year 2021.
+    </div>
+
+    <div className="icons">
+      <IconLink iconName="github" link="https://github.com/Michael-Gatmaitan" />
+      <IconLink iconName="facebook" link="https://www.facebook.com/michael.gatmaitan29" />
+      <IconLink iconName="instagram" link="https://www.instagram.com/mchlgtmtn/" />
+      <IconLink iconName="twitter" link="https://twitter.com/MichaelGatmait3" />
+      <IconLink iconName="portfolio" link="https://www.google.com/" />
+    </div>
   </div>
 );
+
+const IconLink = ({ iconName, link }) => {
+
+  let capitalizedIconName = iconName.charAt(0).toUpperCase() + iconName.slice(1);
+  
+  return (
+    <a className="icon-link" href="link">
+      <div className="icon-container">
+        <img src={`./svg_icons/${iconName}.svg`} width="38" alt="" />
+      </div>
+      <div className="textlink-container">
+        <div href={link}>{capitalizedIconName}</div>
+      </div>
+    </a>
+  );
+}
 
 export default Contact;
